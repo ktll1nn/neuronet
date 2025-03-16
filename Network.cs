34b815708ddbc6689c2c539b20d29f88c4b0402a
Ekaterina Sviridova <sviridovaekaterina89@gmail.com>
2025-03-16 21:26:31 +0300

    Add files via upload

---
+++ Network.cs
@@ -0,0 +1,131 @@
+﻿using NeuroNet_17;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroNet_17
+{
+    class Network
+    {
+        struct LayerT
+        {
+            public Vector x; // вход слоя
+            public Vector z; // активированный выход слоя
+            public Vector df; // производная функции активации слоя
+        }
+        Matrix[] weights; // матрицы весов слоя
+        LayerT[] L; // значения на каждом слое
+        Vector[] deltas; // дельты ошибки на каждом слое
+        int layersN; // число слоёв
+        public Network(int[] sizes)
+        {
+            // создаём генератор случайных чисел
+            Random random = new Random(DateTime.Now.Millisecond);
+            layersN = sizes.Length - 1; // запоминаем число слоёв
+            weights = new Matrix[layersN]; // создаём массив матриц весовых коэффициентов
+            L = new LayerT[layersN]; // создаём массив значений на каждом слое
+            deltas = new Vector[layersN]; // создаём массив для дельт
+            for (int k = 1; k < sizes.Length; k++)
+            {
+                // создаём матрицу весовых коэффициентов
+                weights[k - 1] = new Matrix(sizes[k], sizes[k - 1], random);
+                L[k - 1].x = new Vector(sizes[k - 1]); // создаём вектор для входа слоя
+                L[k - 1].z = new Vector(sizes[k]); // создаём вектор для выхода слоя
+                L[k - 1].df = new Vector(sizes[k]); // создаём вектор для производной слоя
+                deltas[k - 1] = new Vector(sizes[k]); // создаём вектор для дельт
+            }
+        }
+        // прямое распространение
+        public Vector Forward(Vector input)
+        {
+            for (int k = 0; k < layersN; k++)
+            {
+                if (k == 0)
+                {
+                    for (int i = 0; i < input.n; i++)
+                        L[k].x[i] = input[i];
+                }
+                else
+                {
+                    for (int i = 0; i < L[k - 1].z.n; i++)
+                        L[k].x[i] = L[k - 1].z[i];
+                }
+                for (int i = 0; i < weights[k].n; i++)
+                {
+                    double y = 0;
+                    for (int j = 0; j < weights[k].m; j++)
+                        y += weights[k][i, j] * L[k].x[j];
+                    // активация с помощью сигмоидальной функции
+                    L[k].z[i] = 1 / (1 + Math.Exp(-y));
+                    L[k].df[i] = L[k].z[i] * (1 - L[k].z[i]);
+                }
+            }
+            return L[layersN - 1].z; // возвращаем результат
+        } // End Forward
+          // обратное распространение
+        void Backward(Vector output, ref double error)
+        {
+            int last = layersN - 1;
+            error = 0; // обнуляем ошибку
+            for (int i = 0; i < output.n; i++)
+            {
+                double e = L[last].z[i] - output[i]; // находим разность значений векторов
+                deltas[last][i] = e * L[last].df[i]; // запоминаем дельту
+                error += e * e / 2; // прибавляем к ошибке половину квадрата значения
+            }
+            // вычисляем каждую предыдущую дельту на основе текущей
+            // с помощью умножения на транспонированную матрицу
+            for (int k = last; k > 0; k--)
+            {
+                for (int i = 0; i < weights[k].m; i++)
+                {
+                    deltas[k - 1][i] = 0;
+                    for (int j = 0; j < weights[k].n; j++)
+                        deltas[k - 1][i] += weights[k][j, i] * deltas[k][j];
+                    // умножаем получаемое значение на производную предыдущего слоя
+                    deltas[k - 1][i] *= L[k - 1].df[i];
+                }
+            }
+        } // End Backward
+          // обновление весовых коэффициентов, alpha - скорость обучения
+        void UpdateWeights(double alpha)
+        {
+            for (int k = 0; k < layersN; k++)
+            {
+                for (int i = 0; i < weights[k].n; i++)
+                {
+                    for (int j = 0; j < weights[k].m; j++)
+                    {
+                        weights[k][i, j] -= alpha * deltas[k][i] * L[k].x[j];
+                    }
+                }
+            }
+        } // End UpdateWeights
+          // тренировка сети
+        public void Train(Vector[] X, Vector[] Y, double alpha, double eps, int epochs)
+        {
+            int epoch = 1; // номер эпохи
+            double error; // ошибка эпохи
+            DateTime startTime = DateTime.Now; // затраченное время
+            do
+            {
+                error = 0; // обнуляем ошибку
+                           // проходимся по всем элементам обучающего множества
+                for (int i = 0; i < X.Length; i++)
+                {
+                    Forward(X[i]); // прямое распространение сигнала
+                    Backward(Y[i], ref error); // обратное распространение ошибки
+                    UpdateWeights(alpha); // обновление весовых коэффициентов
+                }
+                // выводим в консоль номер эпохи и величину ошибки
+                Console.CursorLeft = 0;
+                Console.Write("Эпоха:{0} Ошибка:{1:0.0000000}", epoch, error);
+                epoch++; // увеличиваем номер эпохи
+            } while (epoch <= epochs && error > eps);
+            TimeSpan trainTime = DateTime.Now - startTime;
+            
+        } // End Train
+    } // End Class Network
+}
---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroNet_17
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            // Значения функции для обучающих выборок
+            double[,] func =
+            {
+               { 0.1, 0.1, 0.99995 },
+               { 0.2, 0.2, 0.999200107 },
+               { 0.3, 0.3, 0.995952733 },
+               { 0.4, 0.4, 0.987227283 },
+               { 0.5, 0.5, 0.968912422 },
+               { 0.6, 0.6, 0.935896824 },
+               { 0.7, 0.7, 0.882332859 },
+               { 0.8, 0.8, 0.802095758 },
+               { 0.9, 0.9, 0.689498433 },
+               { 1.0, 1.0, 0.540302306 }
+           };
+
+            // Значения функции для тестовой выборки
+            double[] tstX = { 0.45 };
+            double[] tstY = { 0.45 };
+            double[] tstZ = { 0.979566842 };
+
+            int h_neuron_num = 10; // кол-во нейронов скрытого слоя
+            int epoch_num = 100000; // кол-во эпох
+            double err_level = 0.0000001; // точность вычислений
+            int study_set_num = func.Length / 3; // кол-во обучающих выборок
+            int varNum = 17;
+            string FIO = "Свиридова Е.К.";
+            string grName = "ИС50-2-22";
+
+            // выполняем нормализацию значений
+            Vector[] X1 = new Vector[study_set_num];
+            Vector[] Y1 = new Vector[study_set_num];
+            for (int i = 0; i < study_set_num; i++) 
+
+            {
+                X1[i] = new Vector(func[i, 0], func[i, 1]);
+                Y1[i] = new Vector(Math.Cos(func[i, 0] * func[i, 1]));
+            };
+
+            Vector[] X2 = { new Vector(tstX[0], tstY[0]) };
+            Vector[] Y2 = { new Vector(Math.Cos(tstX[0] * tstY[0])) };
+
+            string outputFile = $"NeuroNet_{varNum}.txt"; // Имя выходного файла
+
+            string outString = "ИСиТ. Пр.р. №3. \"Простейшая нейросеть\"\n" +
+            "Выполнил : " + FIO + "\n" +
+            "Группа : " + grName + "\n" +
+            "Вариант №: " + varNum.ToString() + "\n" +
+            "------------------------------------\n\n" +
+            "Параметры:\n" +
+            "- Количество обучающих выборок: " + study_set_num.ToString() + "\n" +
+            "- Количество нейронов в скрытом слое: " + h_neuron_num.ToString() + "\n" +
+            "- Количество эпох: " + epoch_num.ToString() + "\n" +
+            "- Ошибка: " + err_level.ToString("F8") + "\n";
+
+            DateTime startTime = DateTime.Now; // Запоминаем время начала выполнения программы
+
+
+            outString += "\nИсходные данные для обучающих выборок" + "\n-------------------------------------" +
+            "\nЗначения X:\tЗначения Y:\tЗначения Z:";
+
+            for (int i = 0; i < study_set_num; i++)
+            {
+                outString += "\n" + func[i, 0].ToString("0.000000");
+                outString += "\t" + func[i, 1].ToString("0.000000");
+                outString += "\t" + func[i, 2].ToString("0.000000");
+            }
+
+            outString += "\n\nИсходные данные для тестовой выборки" + "\n------------------------------------" +
+            "\nТестовое значение X = " + tstX[0].ToString("0.000000") +
+            "\nТестовое значение Y = " + tstY[0].ToString("0.000000") +
+            "\nТестовое значение Z = " + tstZ[0].ToString("0.000000") + "\n";
+
+            //вывод данных на экран и запись в файл
+            Console.WriteLine(outString);
+
+            // создаём сеть с двумя входами, N нейронами в скрытом слое и одним выходом
+            Network network = new Network(new int[] { 2, h_neuron_num, 1 });
+            outString = "Обучение нейросети\n" + "------------------";
+            Console.WriteLine(outString);
+
+            // запускаем обучение сети
+            network.Train(X1, Y1, 0.5, err_level, epoch_num);
+            outString = "\n\nОбучающие выборки и результаты расчета:\n" + "--------------------------------------";
+            Console.WriteLine(outString);
+
+            for (int i = 0; i < study_set_num; i++)
+
+            {
+                {
+                    // запускаем сеть с обучающими выборками
+                    Vector output = network.Forward(X1[i]);
+                    // выводим результаты расчетов на каждой обучающей выборке
+                    Console.WriteLine("X1: {0:0.0000000} {1:0.0000000}\t Y1: {2:0.0000000}\t результат: {3:0.0000000}", X1[i][0], X1[i][1], Math.Cos(X1[i][0] * X1[i][1]), output[0]);
+                }
+
+                // запускаем сеть с тестовой выборкой
+                Vector output2 = network.Forward(X2[0]);
+                // выводим результаты расчетов на тестовой выборке
+                outString = "\nТестовая выборка и результат расчета:\n" +
+                "-------------------------------------";
+                Console.WriteLine(outString);
+                Console.WriteLine("X2: {0:0.0000000} {1:0.0000000}\t Y2: {2:0.0000000}\t результат: {3:0.0000000}", X2[0][0], X2[0][1], Math.Cos(X2[0][0] * X2[0][1]), output2[0]);
+                outString = "\nИтоги:" + "\n------" + "\nВычисленное нейросетью значение функции Z = " + Math.Cos(X2[0][0] * X2[0][1]).ToString("0.000000") + "\nРазность между тестовым и рассчитанным значениями Z = " +
+
+                Math.Abs(Math.Cos(X2[0][0] * X2[0][1]) - output2[0]).ToString("0.000000");
+                TimeSpan executionTime = DateTime.Now - startTime;
+                Console.WriteLine(outString);
+
+                Console.WriteLine($"\nЗатраченное время: {executionTime.TotalSeconds:F6} секунд");
+                Console.WriteLine("\nНажмите любую клавишу...");
+                Console.ReadKey();
+
+            }// End Main
+
+
+        }
+
+    }
+}
